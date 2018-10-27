8ace4ddea6a7949eefb7d6a5f0f977c7e66f6715
Mamadou Diallo <mamadou.diallo@wework.com>
2018-10-27 02:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+
+  const strs= []
+  for(var counter = 0; counter<musicians.length; counter++){
+    const str = musicians[counter] +" plays " + instruments[counter]
+    console.log(str)
+strs.push(str)
+  }
+return strs
+}
+ function  johnLennonFacts(facts){
+   const strs = ["!!!"];
+   
+   }
+   
+   function iLoveTheBeatles(number){
+     const num = []
+     
+     
+   }
+  
+  
+
+  
+   
+ 
+  
+
+
+
+  
+   
+
+
+     
+   
+ 
